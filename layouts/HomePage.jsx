557626747d0605b6e49bc9a809865105a4064fa3
Anthony Acosta <anthony082008@hotmail.es>
2025-03-19 23:20:47 -0500

    First retouches drive document

--- layouts/HomePage.jsx
+++ layouts/HomePage.jsx
@@ -1,6 +1,10 @@
+"use client";
+import MotionSection from "@/components/MotionSection";
+
+// Sections views
 import Hero from "@/views/home/sections/Hero";
-import OurProcess from "@/views/shared/OurProcess";
 import Overview from "@/views/home/sections/Overview";
+import OurProcess from "@/views/shared/OurProcess";
 import Separator from "@/views/shared/Separator";
 import Testimonials from "@/views/shared/Testimonials";
 import ContactForm from "@/views/home/sections/ContactForm";
@@ -11,17 +15,29 @@ export default function HomePage() {
     <main>
       <Hero />
 
-      <Overview />
+      <MotionSection>
+        <Overview />
+      </MotionSection>
 
-      <OurProcess />
+      <MotionSection>
+        <OurProcess />
+      </MotionSection>
 
-      <Separator />
+      <MotionSection delay={0.3}>
+        <Separator />
+      </MotionSection>
 
-      <OurServicesHome backgroundLines="true" />
+      <MotionSection>
+        <OurServicesHome backgroundLines="true" />
+      </MotionSection>
 
-      <Testimonials />
+      <MotionSection>
+        <Testimonials />
+      </MotionSection>
 
-      <ContactForm />
+      <MotionSection>
+        <ContactForm />
+      </MotionSection>
     </main>
   );
 }
