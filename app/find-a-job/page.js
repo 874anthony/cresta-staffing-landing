77d15ce312f9c3032dc47281e9974619d064a2be
Anthony Acosta <anthony082008@hotmail.es>
2025-04-01 11:04:12 -0500

    Fix error pt2

--- app/find-a-job/page.js
+++ app/find-a-job/page.js
@@ -1,5 +1,12 @@
+"use client";
+import { Suspense } from "react";
+
 import FindJobPage from "@/layouts/FindJobPage";
 
 export default function FindJob() {
-  return <FindJobPage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <FindJobPage />
+    </Suspense>
+  );
 }
