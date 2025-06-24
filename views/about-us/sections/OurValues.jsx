9bf23e4980a5c9b1d2451a6559dc60ac45b4ff8d
Anthony Acosta <anthony082008@hotmail.es>
2025-06-24 16:20:46 -0500

    Our values added to about us page with carousel in mobile

--- views/about-us/sections/OurValues.jsx
+++ views/about-us/sections/OurValues.jsx
@@ -1,50 +1,78 @@
+import { useState } from "react";
+import useIsMobile from "@/hooks/useIsMobile";
+import { useSwipeable } from "react-swipeable";
+
 import styles from "./OurValues.module.scss";
 
+const values = [
+  {
+    id: "01",
+    title: "Commitment",
+    description:
+      "We deliver quality service and support to both clients and employees, ensuring satisfaction.",
+    illustration: "/assets/about/values/illustration_commitment.png",
+  },
+  {
+    id: "02",
+    title: "Responsibility",
+    description:
+      "We prioritize our team's well-being and development, and the success of our clients' projects.",
+    illustration: "/assets/about/values/illustration_responsibility.png",
+  },
+  {
+    id: "03",
+    title: "Excellence",
+    description:
+      "We aim for excellence in our staffing and operations, always exceeding expectations.",
+    illustration: "/assets/about/values/illustration_excellence.png",
+  },
+  {
+    id: "04",
+    title: "Trust",
+    description:
+      "We build lasting relationships based on trust, transparency, and mutual respect with all our stakeholders.",
+    illustration: "/assets/about/values/illustration_trust.png",
+  },
+  {
+    id: "05",
+    title: "Safety",
+    description:
+      "We prioritize safety in all our operations, ensuring a secure and healthy work environment for everyone.",
+    illustration: "/assets/about/values/illustration_safety.png",
+  },
+  {
+    id: "06",
+    title: "Adaptability",
+    description:
+      "We embrace change and innovation, adapting quickly to meet evolving client needs and market demands.",
+    illustration: "/assets/about/values/illustration_adaptability.png",
+  },
+];
+
 export default function OurValues() {
-  const values = [
-    {
-      id: "01",
-      title: "Commitment",
-      description:
-        "We deliver quality service and support to both clients and employees, ensuring satisfaction.",
-      illustration: "/assets/about/values/illustration_commitment.png",
+  const isMobile = useIsMobile(475);
+  const slides = values.flat().map((value) => [value]);
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => {
+      if (currentSlide < slides.length - 1) {
+        setCurrentSlide(currentSlide + 1);
+      }
     },
-    {
-      id: "02",
-      title: "Responsibility",
-      description:
-        "We prioritize our team's well-being and development, and the success of our clients' projects.",
-      illustration: "/assets/about/values/illustration_responsibility.png",
+    onSwipedRight: () => {
+      if (currentSlide > 0) {
+        setCurrentSlide(currentSlide - 1);
+      }
     },
-    {
-      id: "03",
-      title: "Excellence",
-      description:
-        "We aim for excellence in our staffing and operations, always exceeding expectations.",
-      illustration: "/assets/about/values/illustration_excellence.png",
-    },
-    {
-      id: "04",
-      title: "Trust",
-      description:
-        "We build lasting relationships based on trust, transparency, and mutual respect with all our stakeholders.",
-      illustration: "/assets/about/values/illustration_trust.png",
-    },
-    {
-      id: "05",
-      title: "Safety",
-      description:
-        "We prioritize safety in all our operations, ensuring a secure and healthy work environment for everyone.",
-      illustration: "/assets/about/values/illustration_safety.png",
-    },
-    {
-      id: "06",
-      title: "Adaptability",
-      description:
-        "We embrace change and innovation, adapting quickly to meet evolving client needs and market demands.",
-      illustration: "/assets/about/values/illustration_adaptability.png",
-    },
-  ];
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true, // optional: allows swipe detection with a mouse
+  });
+
+  const handleDotClick = (index) => {
+    setCurrentSlide(index);
+  };
 
   return (
     <section className={styles.our__values}>
@@ -55,21 +83,78 @@ export default function OurValues() {
         inspire us to do better.
       </p>
 
-      <div className={styles.our__valuesGrid}>
-        {values.map((value) => (
-          <div key={value.id} className={styles.valueCard}>
-            <span className={styles.valueCard__number}>{value.id}</span>
-            <h4 className={styles.valueCard__title}>{value.title}</h4>
-            <div className={styles.valueCard__illustration}>
-              <img
-                src={value.illustration}
-                alt={`${value.title} illustration`}
-              />
+      {!isMobile && (
+        <div className={styles.our__valuesGrid}>
+          {values.map((value) => (
+            <div key={value.id} className={styles.valueCard}>
+              <span className={styles.valueCard__number}>{value.id}</span>
+              <h4 className={styles.valueCard__title}>{value.title}</h4>
+              <div className={styles.valueCard__illustration}>
+                <img
+                  src={value.illustration}
+                  alt={`${value.title} illustration`}
+                />
+              </div>
+              <p className={styles.valueCard__description}>
+                {value.description}
+              </p>
             </div>
-            <p className={styles.valueCard__description}>{value.description}</p>
+          ))}
+        </div>
+      )}
+
+      {isMobile && (
+        <>
+          <div className={styles.containerMobile}>
+            <div className={styles.carouselContainer} {...swipeHandlers}>
+              <div
+                className={styles.carouselTrack}
+                style={{
+                  transform: `translateX(-${
+                    currentSlide * (100 / slides.length)
+                  }%)`,
+                }}
+              >
+                {slides.map((slide, slideIndex) => (
+                  <div key={slideIndex} className={styles.carouselSlide}>
+                    {slide.map((value) => (
+                      <div key={value.id} className={styles.valueCard}>
+                        <span className={styles.valueCard__number}>
+                          {value.id}
+                        </span>
+                        <h4 className={styles.valueCard__title}>
+                          {value.title}
+                        </h4>
+                        <div className={styles.valueCard__illustration}>
+                          <img
+                            src={value.illustration}
+                            alt={`${value.title} illustration`}
+                          />
+                        </div>
+                        <p className={styles.valueCard__description}>
+                          {value.description}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className={styles.carouselDots}>
+              {slides.map((_, index) => (
+                <span
+                  key={index}
+                  className={`${styles.dot} ${
+                    currentSlide === index ? styles.active : ""
+                  }`}
+                  onClick={() => handleDotClick(index)}
+                ></span>
+              ))}
+            </div>
           </div>
-        ))}
-      </div>
+        </>
+      )}
     </section>
   );
 }
